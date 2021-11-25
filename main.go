1b900a637d33906a7725b8bb3e0ef688b1a555c1
Rob David <rob.j.david@googlemail.com>
2021-11-25 22:30:06 +0000

    Make switch to Linux persistant

--- main.go
+++ main.go
@@ -9,78 +9,78 @@ import (
 	"strings"
 )
 
-func pauseFatal(output string){
+func pauseFatal(output string) {
 	fmt.Println(output)
 	fmt.Print(`Enter to exit:`)
 	fmt.Scanln()
 	os.Exit(0)
 }
 
-func main(){
+func main() {
 	shell, err := powershell.New(&backend.Local{})
 	if err != nil {
 		log.Panicln("Open Powershell failed:", err.Error())
 	}
 	defer shell.Exit()
-	stdout,_,err:=shell.Execute("bcdedit /enum firmware")
-	if err!=nil{
+	stdout, _, err := shell.Execute("bcdedit /enum firmware")
+	if err != nil {
 		log.Panicln("Execute Error:", err.Error())
 	}
-	windowsBootManager:="{bootmgr}"
-	if !strings.Contains(stdout,windowsBootManager){
+	windowsBootManager := "{bootmgr}"
+	if !strings.Contains(stdout, windowsBootManager) {
 		pauseFatal(stdout)
 	}
 
-	firmwareLines:=strings.Split(stdout,"\r\n")
-	UEFIBootList:=make(map[string]string)
+	firmwareLines := strings.Split(stdout, "\r\n")
+	UEFIBootList := make(map[string]string)
 	var identifierList []string
-	currentIdentifier :=""
-	for _,line:=range firmwareLines{
-		if len(line)==0 || line[0]=='-'{
+	currentIdentifier := ""
+	for _, line := range firmwareLines {
+		if len(line) == 0 || line[0] == '-' {
 			continue
 		}
-		lineSplit:=strings.Split(line,`  `)
+		lineSplit := strings.Split(line, `  `)
 		switch lineSplit[0] {
 		case "identifier":
-			currentIdentifier =lineSplit[len(lineSplit)-1]
-			identifierList=append(identifierList,currentIdentifier)
+			currentIdentifier = lineSplit[len(lineSplit)-1]
+			identifierList = append(identifierList, currentIdentifier)
 		case "description":
-			description:=lineSplit[len(lineSplit)-1]
-			description=strings.Trim(description,` `)
-			UEFIBootList[currentIdentifier]=description
+			description := lineSplit[len(lineSplit)-1]
+			description = strings.Trim(description, ` `)
+			UEFIBootList[currentIdentifier] = description
 		}
 	}
 
 	fmt.Println("----UEFI Boot List----")
-	for k,v:=range identifierList{
-		if k==0{
+	for k, v := range identifierList {
+		if k == 0 {
 			continue
 		}
-		fmt.Println(k,UEFIBootList[v])
+		fmt.Println(k, UEFIBootList[v])
 	}
 
 	var inputIndex int
-	defaultIndex:=1
-	for k,v:=range identifierList{
-		if k==0{
+	defaultIndex := 1
+	for k, v := range identifierList {
+		if k == 0 {
 			continue
 		}
-		if strings.Contains(UEFIBootList[v],"Windows Boot Manager"){
+		if strings.Contains(UEFIBootList[v], "Windows Boot Manager") {
 			continue
 		}
-		if strings.Contains(UEFIBootList[v],"Internal"){
+		if strings.Contains(UEFIBootList[v], "Internal") {
 			continue
 		}
-		defaultIndex=k
+		defaultIndex = k
 		break
 	}
-	INPUT:
-	fmt.Printf("\nPlease input index of the next boot, Enter [%s]:",UEFIBootList[identifierList[defaultIndex]])
-	_,err=fmt.Scanln(&inputIndex)
-	if err!=nil{
+INPUT:
+	fmt.Printf("\nPlease input index of the next boot, Enter [%s]:", UEFIBootList[identifierList[defaultIndex]])
+	_, err = fmt.Scanln(&inputIndex)
+	if err != nil {
 		switch err.Error() {
 		case "unexpected newline":
-			inputIndex=defaultIndex
+			inputIndex = defaultIndex
 			break
 		case "EOF":
 			os.Exit(0)
@@ -89,35 +89,37 @@ func main(){
 			goto INPUT
 		}
 	}
-	if inputIndex>=len(identifierList){
+	if inputIndex >= len(identifierList) {
 		fmt.Println("Invalid Index")
 		goto INPUT
 	}
 
-	setNextBoot(shell,identifierList[inputIndex])
-	fmt.Println("Next boot:",UEFIBootList[identifierList[inputIndex]])
+	setNextBoot(shell, identifierList[inputIndex])
+	fmt.Println("Next boot:", UEFIBootList[identifierList[inputIndex]])
 
 	fmt.Print("Enter to reboot:")
-	_,err=fmt.Scanln(&inputIndex)
-	if err!=nil && err.Error()=="unexpected newline"{
+	_, err = fmt.Scanln(&inputIndex)
+	if err != nil && err.Error() == "unexpected newline" {
 		rebootNow(shell)
 	}
 
 }
 
-func setNextBoot(shell powershell.Shell,identifier string){
-	cmd:=fmt.Sprintf(`bcdedit /set "{fwbootmgr}" bootsequence "%s"`,identifier)
-	stdout,_,err:=shell.Execute(cmd)
-	if err!=nil{
+func setNextBoot(shell powershell.Shell, identifier string) {
+	//cmd := fmt.Sprintf(`bcdedit /set "{fwbootmgr}" bootsequence "%s"`, identifier)
+	cmd := fmt.Sprintf(`bcdedit /set "{fwbootmgr}" displayorder "%s" /addfirst`, identifier)
+	fmt.Println(cmd)
+	stdout, _, err := shell.Execute(cmd)
+	if err != nil {
 		pauseFatal(err.Error())
 	}
 	fmt.Print(stdout)
 }
 
-func rebootNow(shell powershell.Shell){
-	cmd:=`shutdown /r /t 0`
-	_,_,err:=shell.Execute(cmd)
-	if err!=nil{
+func rebootNow(shell powershell.Shell) {
+	cmd := `shutdown /r /t 0`
+	_, _, err := shell.Execute(cmd)
+	if err != nil {
 		pauseFatal(err.Error())
 	}
 }
